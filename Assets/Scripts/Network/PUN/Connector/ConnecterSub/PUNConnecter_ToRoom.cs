73ef43ee79576cec1a3daebfe8cb89d8d77dc040
thomaskaoxrspace <thomas.kao@xrspace.io>
2020-09-24 18:38:19 +0800

    Default RoomName

--- Assets/Scripts/Network/PUN/Connector/ConnecterSub/PUNConnecter_ToRoom.cs
+++ Assets/Scripts/Network/PUN/Connector/ConnecterSub/PUNConnecter_ToRoom.cs
@@ -60,6 +60,9 @@ public partial class PUNConnecter : MonoBehaviourPunCallbacks
             PublishUserId = true
         };
         joinRoomResult = new TaskCompletionSource<bool>();
+
+        if (string.IsNullOrEmpty(roomName))
+            roomName = "Default";
         if (!PhotonNetwork.JoinOrCreateRoom(roomName, rooOpt, TypedLobby.Default)) // Will callback: OnJoinedRoom or OnJoinRoomFailed.
         {
             Debug.LogWarning($"{scriptName} JoinGameRoom JoinOrCreateRoom Immediately FAIL");
