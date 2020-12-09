75efea509fffc11e6c372006eb69cdf321e637f5
thomas.kao <thomas.kao@xrspace.io>
2020-12-09 12:09:09 +0800

    refactor:using SingletonMonoBehaviour

--- Assets/Scripts/Manager/ServiceManager.cs
+++ Assets/Scripts/Manager/ServiceManager.cs
@@ -2,31 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ServiceManager : MonoBehaviour
+public class ServiceManager : SingletonMonoBehaviour<ServiceManager>
 {
     public NetworkSystem networkSystem;
     public PlayerManager playerManager;
-
-    static ServiceManager instance;
-    public static ServiceManager Instance {
-        get
-        {
-            if (instance == null)
-            {
-                var go = GameObject.Find("ServiceManager");
-                if(go != null)
-                    instance = go.GetComponent<ServiceManager>();
-            }
-
-            return instance;
-        }
-    }
-
-    public void Awake()
-    {
-        if (instance != null)
-            Destroy(gameObject);
-
-        DontDestroyOnLoad(this);
-    }
 }
