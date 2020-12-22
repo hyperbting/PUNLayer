0a4543d6c3d680bfa99afb0d23f4ec23dc443030
thomas.kao <thomas.kao@xrspace.io>
2020-12-22 18:28:58 +0800

    feat(avatar tool): detection around designated area

---
+++ Assets/HeadDetectable.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class HeadDetectable : MonoBehaviour, IHeadDetectable
+{
+
+    public Action<bool, Vector3> OnRelativePositionChanged {
+        get;
+        set;
+    }
+
+    [SerializeField] Guid myGUID = Guid.NewGuid();
+    public Guid GetUUID
+    {
+        get
+        {
+            return myGUID;
+        }
+    }
+
+    [SerializeField] ControllerType conType;
+    public ControllerType ConType
+    {
+        get
+        {
+            return conType;
+        }
+    }
+
+    [Header("Debug Purpose")]
+    [SerializeField] bool inRange;
+    [SerializeField] Vector3 deltaPosition;
+
+    [SerializeField] HeadDetection hd;
+    public void Assign(HeadDetection hd)
+    {
+        this.hd = hd;
+    }
+
+    public void Clean()
+    {
+        this.hd = null;
+    }
+
+    public bool InsideHeadDetection(out Vector3 posDelta)
+    {
+        posDelta = Vector3.zero;
+
+        if (hd == null)
+            return false;
+
+        var delta = hd.TryGetPositionDelta(this);
+        //// not in range; Unregister
+        if (posDelta == null)
+        {
+            return false;
+        }
+
+        posDelta = (Vector3)delta;
+        return true;
+    }
+
+    void ChangeRelativePosition(bool enabled, Vector3 deltaPos)
+    {
+    }
+
+    void UpdateDetection()
+    {
+        if (InsideHeadDetection(out Vector3 posDelta))
+        {
+            if (!inRange || Vector3.Distance(deltaPosition, posDelta)>0.1)
+                OnRelativePositionChanged(true, posDelta);
+
+            inRange = true;
+            deltaPosition = posDelta;
+        }
+        else
+        {
+            if (inRange)
+            {
+                OnRelativePositionChanged(false, Vector3.zero);
+
+                inRange = false;
+                deltaPosition = posDelta;
+            }
+        }
+    }
+
+    #region Mono
+    private void OnEnable()
+    {
+        OnRelativePositionChanged += ChangeRelativePosition;
+    }
+
+    private void OnDisable()
+    {
+        OnRelativePositionChanged -= ChangeRelativePosition;
+    }
+
+    private void Update()
+    {
+        UpdateDetection();
+    }
+    #endregion
+}
+
+public enum ControllerType
+{
+    None,
+    Left,
+    Right
+}
---
+++ Assets/HeadDetection.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class HeadDetection : MonoBehaviour
+{
+    Dictionary<System.Guid, (GameObject,Vector3)> detectedIHeadDetectable = new Dictionary<System.Guid, (GameObject, Vector3)>();
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var itf = other.gameObject.GetComponent<IHeadDetectable>();
+        if (itf == null)
+            return;
+
+        itf.Assign(this);
+        detectedIHeadDetectable[itf.GetUUID] = (other.gameObject, Vector3.zero);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        var itf = other.gameObject.GetComponent<IHeadDetectable>();
+        if (itf == null)
+            return;
+
+        UnRegister(itf);
+    }
+
+    public void UnRegister(IHeadDetectable itf)
+    {
+        itf.Clean();
+        detectedIHeadDetectable.Remove(itf.GetUUID);
+    }
+
+    private void LateUpdate()
+    {
+        var keyList = new List<System.Guid>(detectedIHeadDetectable.Keys);
+        foreach (var key in keyList)
+        {
+            var val = detectedIHeadDetectable[key];
+            var deltaPos = val.Item1.transform.position - transform.position;
+            detectedIHeadDetectable[key] = (val.Item1, deltaPos);
+        }
+    }
+
+    public Vector3? TryGetPositionDelta(IHeadDetectable ihd)
+    {
+        if (detectedIHeadDetectable.TryGetValue(ihd.GetUUID, out (GameObject, Vector3) val))
+        {
+            return val.Item2;
+        }
+
+        return null;
+    }
+}
+
+public interface IHeadDetectable
+{
+    System.Guid GetUUID { get; }
+    ControllerType ConType { get; }
+
+    System.Action<bool, Vector3> OnRelativePositionChanged { get; set; }
+
+    void Assign(HeadDetection hd);
+    void Clean();
+}
+
