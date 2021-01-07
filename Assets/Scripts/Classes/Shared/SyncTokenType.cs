3bdb354a8486a188abae3a41879d338b26fa3da4
thomas.kao <thomas.kao@xrspace.io>
2021-01-07 11:50:26 +0800

    refactor: detailed commit on TokenType.

--- Assets/Scripts/Classes/Shared/SyncTokenType.cs
+++ Assets/Scripts/Classes/Shared/SyncTokenType.cs
@@ -1,14 +1,23 @@
 ﻿public enum SyncTokenType
 {
     Unknown,
+    /// <summary>
+    /// Scene object, won't disappear when owner gone, disappear when LeftRoom
+    /// </summary>
     General,
     /// <summary>
-    /// Owner Creation
+    /// Player Object, disappear when owner gone, disappear when LeftRoom
+    /// Structure is PlayerDataCreateion below TransmissionToken
     /// </summary>
     Player,
     /// <summary>
-    /// SceneOwned, NOT destroy when controller left, countdown to destroy; 
-    /// NOT NetworkInstantiate BUT RaiseEvent, MC Clean RaiseEvent when countdown Finished
+    /// LOCAL Object, NOT destroy because of LeftRoom or ControllerLeft
+    /// Structure is PlayerDataCreateion and TransmissionToken side by side.
+    /// Two Parts Composition:
+    /// 1. SemiPersistenceManager create/control DataCreateion
+    /// 2. DataCreateion link to TransmissionToken; TransmissionToken gone when not in room.
+    /// Whenever DataCreateion cannot find a TransmissionToken linked, Countdown to destroy.
+    /// whenever Someone request a DataCreateion, SemiPersistenceManager find in the list or create a local DataCreateion.
     /// </summary>
-    Persistence,
+    SemiPersistence,
 }
