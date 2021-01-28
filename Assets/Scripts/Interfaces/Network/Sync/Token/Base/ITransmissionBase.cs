61c620b5458121bb356b53cd2a96476a4791e2c1
thomas.kao <thomas.kao@xrspace.io>
2021-01-28 11:32:54 +0800

    refactor: relocate interface

---
+++ Assets/Scripts/Interfaces/Network/Sync/Token/Base/ITransmissionBase.cs
@@ -0,0 +1,16 @@
+﻿// ITokenHandler Request one of this From ITokenProvider When OnJoinedRoom
+public interface ITransmissionBase
+{
+    /// <summary>
+    /// Setup By TokenHandler locally or ITransmissionBase remotely
+    /// </summary>
+    /// <param name="insData"></param>
+    /// <param name="tokenUser"></param>
+    void Setup(InstantiationData insData, ISyncHandlerUser tokenUser = null);
+
+    ISerializableHelper SeriHelper { get; }
+    ISerializableHelper StatHelper { get; }
+
+    void Register(params SerializableReadWrite[] srws);
+    void Unregister(params SerializableReadWrite[] srws);
+}
