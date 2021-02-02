13c782c68dbbac93e468a7d91a9ff49c20357680
thomas.kao <thomas.kao@xrspace.io>
2021-02-02 17:41:16 +0800

    refactor: from single Action<object>/ Func<object> to array

--- Assets/Scripts/Network/PUN/SyncHelper/SerializableHelper.cs
+++ Assets/Scripts/Network/PUN/SyncHelper/SerializableHelper.cs
@@ -12,33 +12,49 @@ public class SerializableHelper : BaseSyncHelper, IPunObservable
         var keys = new List<string>(dataToSync.Keys);
         if (stream.IsWriting)
         {
-            //Debug.Log($"IsWriting {keys.Count}");
-            for (int i = 0; i < keys.Count; i++)
+            SendToRemote(keys, stream);
+        }
+        else
+        {
+            ReadToLocal(keys, stream);
+        }
+    }
+    #endregion
+
+    void SendToRemote(List<string> keys, PhotonStream stream)
+    {
+        //Debug.Log($"IsWriting {keys.Count}");
+        for (int i = 0; i < keys.Count; i++)
+        {
+            //Debug.Log($"TryGetValue for Key:{keys[i]}");
+            if (dataToSync.TryGetValue(keys[i], out SerializableReadWrite val))
             {
-                //Debug.Log($"TryGetValue for Key:{keys[i]}");
-                if (dataToSync.TryGetValue(keys[i], out SerializableReadWrite val))
+                for (int j = 0; j < val.Read.Length; j++)
                 {
-                    var va = val?.Read();
-                    //Debug.Log($" Key:{keys[i]} {va}");
+                    var va = val?.Read[j]();
+                    //Debug.Log($" Key:{keys[i]}-{j} {va}");
                     stream.SendNext(va);
                 }
             }
         }
-        else
+    }
+
+    void ReadToLocal(List<string> keys, PhotonStream stream)
+    {
+        //Debug.Log($"IsReading {keys.Count}");
+        for (int i = 0; i < keys.Count; i++)
         {
-            //Debug.Log($"IsReading {keys.Count}");
-            for (int i = 0; i < keys.Count; i++)
+            //Debug.Log($"TryGetValue for Key:{keys[i]}");
+            if (dataToSync.TryGetValue(keys[i], out SerializableReadWrite val))
             {
-                //Debug.Log($"TryGetValue for Key:{keys[i]}");
-                if (dataToSync.TryGetValue(keys[i], out SerializableReadWrite  val))
+                for (int j = 0; j < val.Write.Length; j++)
                 {
                     var va = stream.ReceiveNext();
-                    //Debug.Log($"{va} Received");
-                    val?.Write(va);
+                    //Debug.Log($"{va}-{j} Received");
+                    val?.Write[j](va);
                 }
             }
         }
     }
-    #endregion
 }
 
