448b42a77b34badc8f90cb230c23111de60cb3d9
thomas.kao <thomas.kao@xrspace.io>
2021-04-15 10:16:06 +0800

    refactor: replace PhotonStatsGui with one not using old Input class

---
+++ Assets/Scripts/Network/PUN/Debug/PhotonStatsGuiWithoutInput.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+using Photon.Pun;
+using Photon.Realtime;
+using ExitGames.Client.Photon;
+
+public class PhotonStatsGuiWithoutInput : Photon.Pun.UtilityScripts.PhotonStatsGui
+{
+
+    // Update is called once per frame
+    public new void Update()
+    {
+    }
+}
